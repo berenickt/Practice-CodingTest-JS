d1aa856a8387b0f815de25ab7d73a71a2d21b830
Seungwoo Yoo <berenickt99@gmail.com>
2023-05-27 05:31:46 +0900

    FIX: 폴더명

--- 07-Baekjoon/01_기초문법/04-기본입력-fs모듈.js
+++ 07-Baekjoon/01_기초문법/04-기본입력-fs모듈.js
@@ -5,7 +5,7 @@
  */
 // readline 모듈보다는 fs를 이용해 파일 전체를 읽어 들여 처리하기
 let fs = require('fs')
-let input = fs.readFileSync('/dev/stdin').toString().split('\n')
-// let input = fs.readFileSync('input.txt').toString().split('\n');
+// let input = fs.readFileSync('/dev/stdin').toString().split('\n')
+let input = fs.readFileSync('input.txt').toString().split('\n')
 
 console.log(input)
